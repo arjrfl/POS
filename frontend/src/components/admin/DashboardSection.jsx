16b96e799dec9559f35ebc218449f6b81082a7e7
rafael <arjayrafaelical@gmail.com>
2026-07-25 02:58:08 +0800

    style: 50/50 grid split for dashboard chart and payment users table

--- frontend/src/components/admin/DashboardSection.jsx
+++ frontend/src/components/admin/DashboardSection.jsx
@@ -141,11 +141,11 @@ export function DashboardSection() {
         />
       </div>
 
-      <div className="flex-1 min-h-0 flex flex-col lg:flex-row gap-4">
-        <div className="flex-[60] h-full min-h-0 flex flex-col">
+      <div className="flex-1 min-h-0 grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <div className="h-full min-h-0 flex flex-col">
           <TopProductsChart />
         </div>
-        <div className="flex-[40] h-full min-h-0 flex flex-col">
+        <div className="h-full min-h-0 flex flex-col">
           <PaymentUserSalesTable fromDate={appliedRange?.from} toDate={appliedRange?.to} rangeLabel={rangeLabel} />
         </div>
       </div>
